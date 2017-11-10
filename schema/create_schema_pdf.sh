5b4ec0f9e00488f55e0f2ad52d7f3538fd7e65e9
Matthias Koenig <konigmatt@googlemail.com>
2017-11-10 17:34:06 +0100

    created latest schema supplement

--- schema/create_schema_pdf.sh
+++ schema/create_schema_pdf.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-# Convert markdown to pdf
+################################################
+# Convert markdown to pdf using pandoc
+################################################
+# requirements:
+#  sudo apt-get install pandoc
+#
+# usage:
+#  ./create_schema_pdf.sh
+################################################
 
+echo "create pdf from markdown"
 pandoc -s -o DFBA_models_in_SBML.pdf DFBA_models_in_SBML.md --variable urlcolor=cyan
 echo "MD --> PDF: DFBA_models_in_SBML.pdf"
+
+echo "copy to supplement"
+cp DFBA_models_in_SBML.pdf ../docs/manuscript/supplement/S1_DFBA_models_in_SBML.pdf
